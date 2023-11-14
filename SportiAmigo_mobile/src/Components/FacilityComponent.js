73ff6e58de22d03006176c046506218124f5496d
MahmutAtia~ <mahmoud.atia.ead@gmail.com>
2023-11-14 18:25:19 +0300

    start facitlty home screen

---
+++ SportiAmigo_mobile/src/Components/FacilityComponent.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { TouchableOpacity , Image} from 'react-native';
+import {  StyleService, Text, useTheme,Icon,Layout, Card } from '@ui-kitten/components';
+// import { useNavigation } from '@react-navigation/core';
+
+const FeatureElement = ({facility}) => {
+//   const navigation = useNavigation();
+  const theme = useTheme();
+    const styles = StyleService.create(styledTheme);
+
+  return (
+   <Card 
+     style={styles.container}
+    // onPress={() => navigation.navigate('FacilityDetailsScreen')}
+    >
+      <Image
+        style={styles.image}
+        source={{
+          uri: facility.facility_images,
+        }}
+      />
+      <Text style={styles.title}>{facility.name}</Text>
+      <Layout style={styles.ratingContainer}>
+        <Icon
+          name="star"
+          style={styles.ratingIcon}
+          fill={theme['color-primary-500']}
+        />
+        <Text style={styles.ratingText}>{facility.rating}</Text>
+      </Layout>
+      <Layout style={styles.offersContainer}>
+        <Text style={styles.grayText}>{facility.offers}</Text>
+      </Layout>
+      <Layout style={styles.addressContainer}>
+        <Icon
+          name="pin"
+          style={styles.mapPinIcon}
+          fill={theme['color-primary-500']}
+        />
+        <Text style={styles.addressText}>{facility.address}</Text>
+      </Layout>
+
+        </Card>
+    
+
+  );
+};
+
+export default FeatureElement;
+
+
+
+
+const styledTheme = {
+    container: {
+      height: '35%',
+      width: "80%",
+      backgroundColor: 'color-basic-100',
+      marginLeft: 8,
+      shadowColor: 'color-basic-800',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      shadowRadius: 4,
+    },
+    image: {
+      height: '70%',
+      width: '100%',
+    },
+    title: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginTop: 8,
+    },
+    ratingContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 4,
+    },
+    ratingText: {
+      fontSize: 12,
+      color: 'color-primary-500',
+      marginLeft: 2,
+    },
+    offersContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 2,
+    },
+    addressContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginTop: 2,
+    },
+    mapPinIcon: {
+      color: 'color-basic-800',
+      size: 20,
+      marginRight: 2,
+    },
+    addressText: {
+      fontSize: 12,
+      color: 'color-primary-500',
+      marginRight: 2,
+    },
+    grayText: {
+      fontSize: 12,
+      color: 'color-basic-800',
+    },
+  };
--- SportiAmigo_mobile/src/Screens/DrawerScreens/SportFacilitiesScreen.js
+++ SportiAmigo_mobile/src/Screens/DrawerScreens/SportFacilitiesScreen.js
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { Layout, List, useTheme } from '@ui-kitten/components';
+import FeatureElement from '../../Components/FacilityComponent'; // Make sure to import your FacilityCard component
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SportFacilitiesScreen = () => {
-  return (
-    <View>
-      <Text>SportFacilitiesScreen</Text>
-    </View>
-  )
-}
+  const theme = useTheme();
 
-export default SportFacilitiesScreen
+  const facilities = [
+    {
+      name: 'Gym X',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      facility_images: 
+      'https://content.jdmagicbox.com/comp/bokaro/j3/9999p6542.6542.220319002739.b4j3/catalogue/cross-fit-multi-gym-bokaro-thermal-bokaro-gyms-lrxpwfn4d3.jpg'
+    },
+    {
+      name: 'Gym Y',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      facility_images:
+        'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3ltc3xlbnwwfHwwfHx8MA%3D%3D',
+    },
+    {
+      name: 'Gym X',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      facility_images: 
+      'https://content.jdmagicbox.com/comp/bokaro/j3/9999p6542.6542.220319002739.b4j3/catalogue/cross-fit-multi-gym-bokaro-thermal-bokaro-gyms-lrxpwfn4d3.jpg'
+    },
+    {
+      name: 'Gym Y',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      facility_images:
+        'https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z3ltc3xlbnwwfHwwfHx8MA%3D%3D',
+    },
+  ];
+
+  const renderItem = ({ item }) => <FeatureElement facility={item} />;
+
+  return (
+    <Layout style={{ flex: 1 }}>
+      <ScrollView
+         scrollEnabled
+        data={facilities}
+        horizontal
+      
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{
+          paddingVertical: 16,
+          paddingHorizontal: 8,
+
+        }}
+        style={{ backgroundColor: theme['color-basic-100'] }}
+      >
+
+      {facilities.map((facility) => (
+        <FeatureElement facility={facility} />
+      ))}
+
+      </ScrollView>
+    </Layout>
+  );
+};
+
+export default SportFacilitiesScreen;
